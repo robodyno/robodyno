1e4da077ebee069fc5aee4eec83df071cdf75b5d
Sotiredagain <483924399@qq.com>
2026-04-16 09:45:04 +0800

    add reset cmd

--- src/robodyno/components/can_bus/ultrasonic_sensor.py
+++ src/robodyno/components/can_bus/ultrasonic_sensor.py
@@ -48,6 +48,7 @@ class UltrasonicSensor(CanBusDevice):
     _CMD_GET_DISTANCE = 0x02
     _CMD_CONFIG_CAN = 0x03
     _CMD_HEARTBEAT = 0x04
+    _CMD_RESET	   = 0X05
 
     def __init__(self, can: CanBus, id_: int = 0x35):
         """Initializes the ultrasonic sensor driver.
